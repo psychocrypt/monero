dafbe33793cdbd780c579ece7c726a80903719d9
psychocrypt <psychocryptHPC@gmail.com>
2020-01-11 22:05:18 +0100

    format code

--- tests/fuzz/signature.cpp
+++ tests/fuzz/signature.cpp
@@ -1,21 +1,21 @@
 // Copyright (c) 2017-2018, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -34,63 +34,65 @@
 #include "wallet/wallet2.h"
 #include "fuzzer.h"
 
-class SignatureFuzzer: public Fuzzer
+class SignatureFuzzer : public Fuzzer
 {
-public:
-  SignatureFuzzer(): Fuzzer(), wallet(cryptonote::TESTNET) {}
-  virtual int init();
-  virtual int run(const std::string &filename);
+  public:
+	SignatureFuzzer() :
+		Fuzzer(),
+		wallet(cryptonote::TESTNET) {}
+	virtual int init();
+	virtual int run(const std::string& filename);
 
-private:
-  tools::wallet2 wallet;
-  cryptonote::account_public_address address;
+  private:
+	tools::wallet2 wallet;
+	cryptonote::account_public_address address;
 };
 
 int SignatureFuzzer::init()
 {
-  static const char * const spendkey_hex = "0b4f47697ec99c3de6579304e5f25c68b07afbe55b71d99620bf6cbf4e45a80f";
-  crypto::secret_key spendkey;
-  epee::string_tools::hex_to_pod(spendkey_hex, spendkey);
+	static const char* const spendkey_hex = "0b4f47697ec99c3de6579304e5f25c68b07afbe55b71d99620bf6cbf4e45a80f";
+	crypto::secret_key spendkey;
+	epee::string_tools::hex_to_pod(spendkey_hex, spendkey);
 
-  try
-  {
-    wallet.init("");
-    wallet.set_subaddress_lookahead(1, 1);
-    wallet.generate("", "", spendkey, true, false);
+	try
+	{
+		wallet.init("");
+		wallet.set_subaddress_lookahead(1, 1);
+		wallet.generate("", "", spendkey, true, false);
 
-    cryptonote::address_parse_info info;
-    if (!cryptonote::get_account_address_from_str_or_url(info, cryptonote::TESTNET, "9uVsvEryzpN8WH2t1WWhFFCG5tS8cBNdmJYNRuckLENFimfauV5pZKeS1P2CbxGkSDTUPHXWwiYE5ZGSXDAGbaZgDxobqDN"))
-    {
-      std::cerr << "failed to parse address" << std::endl;
-      return 1;
-    }
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << "Error on SignatureFuzzer::init: " << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
+		cryptonote::address_parse_info info;
+		if(!cryptonote::get_account_address_from_str_or_url(info, cryptonote::TESTNET, "9uVsvEryzpN8WH2t1WWhFFCG5tS8cBNdmJYNRuckLENFimfauV5pZKeS1P2CbxGkSDTUPHXWwiYE5ZGSXDAGbaZgDxobqDN"))
+		{
+			std::cerr << "failed to parse address" << std::endl;
+			return 1;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error on SignatureFuzzer::init: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
 
-int SignatureFuzzer::run(const std::string &filename)
+int SignatureFuzzer::run(const std::string& filename)
 {
-  std::string s;
+	std::string s;
 
-  if (!epee::file_io_utils::load_file_to_string(filename, s))
-  {
-    std::cout << "Error: failed to load file " << filename << std::endl;
-    return 1;
-  }
+	if(!epee::file_io_utils::load_file_to_string(filename, s))
+	{
+		std::cout << "Error: failed to load file " << filename << std::endl;
+		return 1;
+	}
 
-  bool valid = wallet.verify("test", address, s);
-  std::cout << "Signature " << (valid ? "valid" : "invalid") << std::endl;
+	bool valid = wallet.verify("test", address, s);
+	std::cout << "Signature " << (valid ? "valid" : "invalid") << std::endl;
 
-  return 0;
+	return 0;
 }
 
-int main(int argc, const char **argv)
+int main(int argc, const char** argv)
 {
-  SignatureFuzzer fuzzer;
-  return run_fuzzer(argc, argv, fuzzer);
+	SignatureFuzzer fuzzer;
+	return run_fuzzer(argc, argv, fuzzer);
 }
--- tests/unit_tests/decompose_amount_into_digits.cpp
+++ tests/unit_tests/decompose_amount_into_digits.cpp
@@ -1,21 +1,21 @@
 // Copyright (c) 2014-2018, The Monero Project
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other
 //    materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //    used to endorse or promote products derived from this software without specific
 //    prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -25,7 +25,7 @@
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #include "gtest/gtest.h"
@@ -35,109 +35,109 @@
 
 #include "cryptonote_basic/cryptonote_format_utils.h"
 
-#define VEC_FROM_ARR(vec)                                               \
-  std::vector<uint64_t> vec;                                            \
-  for (size_t i = 0; i < sizeof(vec##_arr) / sizeof(vec##_arr[0]); ++i) \
-  {                                                                     \
-    vec.push_back(vec##_arr[i]);                                        \
-  }
+#define VEC_FROM_ARR(vec)                                                \
+	std::vector<uint64_t> vec;                                           \
+	for(size_t i = 0; i < sizeof(vec##_arr) / sizeof(vec##_arr[0]); ++i) \
+	{                                                                    \
+		vec.push_back(vec##_arr[i]);                                     \
+	}
 
 namespace
 {
-  struct chunk_handler_t
-  {
-    void operator()(uint64_t chunk) const
-    {
-      m_chunks.push_back(chunk);
-    }
+struct chunk_handler_t
+{
+	void operator()(uint64_t chunk) const
+	{
+		m_chunks.push_back(chunk);
+	}
 
-    mutable std::vector<uint64_t> m_chunks;
-  };
+	mutable std::vector<uint64_t> m_chunks;
+};
 
-  struct dust_handler_t
-  {
-    dust_handler_t()
-      : m_dust(0)
-      , m_has_dust(false)
-    {
-    }
+struct dust_handler_t
+{
+	dust_handler_t() :
+		m_dust(0),
+		m_has_dust(false)
+	{
+	}
 
-    void operator()(uint64_t dust) const
-    {
-      m_dust = dust;
-      m_has_dust = true;
-    }
+	void operator()(uint64_t dust) const
+	{
+		m_dust = dust;
+		m_has_dust = true;
+	}
 
-    mutable uint64_t m_dust;
-    mutable bool m_has_dust;
-  };
+	mutable uint64_t m_dust;
+	mutable bool m_has_dust;
+};
 
-  class decompose_amount_into_digits_test : public ::testing::Test
-  {
+class decompose_amount_into_digits_test : public ::testing::Test
+{
   protected:
-    chunk_handler_t m_chunk_handler;
-    dust_handler_t m_dust_handler;
-  };
-}
+	chunk_handler_t m_chunk_handler;
+	dust_handler_t m_dust_handler;
+};
+} // namespace
 
 TEST_F(decompose_amount_into_digits_test, is_correct_0)
 {
-  std::vector<uint64_t> expected_chunks;
-  cryptonote::decompose_amount_into_digits(0, 0, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_has_dust, false);
+	std::vector<uint64_t> expected_chunks;
+	cryptonote::decompose_amount_into_digits(0, 0, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_has_dust, false);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_1)
 {
-  std::vector<uint64_t> expected_chunks;
-  cryptonote::decompose_amount_into_digits(0, 10, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_has_dust, false);
+	std::vector<uint64_t> expected_chunks;
+	cryptonote::decompose_amount_into_digits(0, 10, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_has_dust, false);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_2)
 {
-  uint64_t expected_chunks_arr[] = {10};
-  VEC_FROM_ARR(expected_chunks);
-  cryptonote::decompose_amount_into_digits(10, 0, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_has_dust, false);
+	uint64_t expected_chunks_arr[] = {10};
+	VEC_FROM_ARR(expected_chunks);
+	cryptonote::decompose_amount_into_digits(10, 0, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_has_dust, false);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_3)
 {
-  std::vector<uint64_t> expected_chunks;
-  uint64_t expected_dust = 10;
-  cryptonote::decompose_amount_into_digits(10, 10, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
+	std::vector<uint64_t> expected_chunks;
+	uint64_t expected_dust = 10;
+	cryptonote::decompose_amount_into_digits(10, 10, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_4)
 {
-  uint64_t expected_dust = 8100;
-  std::vector<uint64_t> expected_chunks;
-  cryptonote::decompose_amount_into_digits(8100, 1000000, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
+	uint64_t expected_dust = 8100;
+	std::vector<uint64_t> expected_chunks;
+	cryptonote::decompose_amount_into_digits(8100, 1000000, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_5)
 {
-  uint64_t expected_chunks_arr[] = {100, 900000, 8000000};
-  VEC_FROM_ARR(expected_chunks);
-  cryptonote::decompose_amount_into_digits(8900100, 10, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_has_dust, false);
+	uint64_t expected_chunks_arr[] = {100, 900000, 8000000};
+	VEC_FROM_ARR(expected_chunks);
+	cryptonote::decompose_amount_into_digits(8900100, 10, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_has_dust, false);
 }
 
 TEST_F(decompose_amount_into_digits_test, is_correct_6)
 {
-  uint64_t expected_chunks_arr[] = {900000, 8000000};
-  VEC_FROM_ARR(expected_chunks);
-  uint64_t expected_dust = 100;
-  cryptonote::decompose_amount_into_digits(8900100, 1000, m_chunk_handler, m_dust_handler);
-  ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
-  ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
+	uint64_t expected_chunks_arr[] = {900000, 8000000};
+	VEC_FROM_ARR(expected_chunks);
+	uint64_t expected_dust = 100;
+	cryptonote::decompose_amount_into_digits(8900100, 1000, m_chunk_handler, m_dust_handler);
+	ASSERT_EQ(m_chunk_handler.m_chunks, expected_chunks);
+	ASSERT_EQ(m_dust_handler.m_dust, expected_dust);
 }
